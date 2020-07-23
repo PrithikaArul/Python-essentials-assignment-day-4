4d0732704fcd826967343620f325558171520b54
PrithikaArul <67939834+PrithikaArul@users.noreply.github.com>
2020-07-23 15:50:22 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Program to find the index of substring in a given string:
+str1="what we think we become; we are python programmers"
+str1.find('we')
+
+
+# In[2]:
+
+
+#Program to find the occurence of all substrings in a given string:
+str1.('we')
+
+
+# In[3]:
+
+
+#Program to illustrate the function islower():
+#Example 1:
+str2="prithika"
+str2.islower()
+
+
+# In[4]:
+
+
+#Example 2:
+str3="Prithika"
+str3.islower()
+#It will return False because the first letter is in uppercase.
+
+
+# In[5]:
+
+
+#Program to illustrate the function isupper():
+#Example 1:
+str4="PRITHIKA"
+str4.isupper()
+
+
+# In[6]:
+
+
+#Example 2:
+str5="PRITHIKa"
+str5.isupper()
+#It will return False because the last letter is in lowercase.
+
